4331ae86bf9cef70a13ae7896bc2bd5411d52c42
kudelandrei <kudel1995@mail.ru>
2017-05-15 14:54:05 +0300

    add change"

--- sourse/index.js
+++ sourse/index.js
@@ -1,3 +1,4 @@
 import createMenu from './menu';
+import '../sass/style.sass';
 var menu = createMenu(['Главная','Блог', 'портфолио'], 'menu');
 document.body.appendChild(menu);
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,41 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+const pug = require('./webpack/pug');
+const devserver = require('./webpack/devserver');
+const sass = require('./webpack/sass');
 
 const PATHS = {
   sourse: path.join(__dirname, 'sourse'), //путь от куда будут браться файлы
   build: path.join(__dirname, 'build') //путь куда будут билдиться проект
 };
 
-module.exports = {
-  entry: PATHS.sourse + '/index.js',
-  output: {
-    path: PATHS.build,
-    filename: '[name].js'
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Webpack app'
-    })
-  ]
+const common = ([
+  {
+    entry: PATHS.sourse + '/index.js',
+    output: {
+      path: PATHS.build,
+      filename: '[name].js'
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Webpack app'
+      })
+    ]
+  }
+  //pug()
+]);
+
+
+module.exports = function(env) {
+  if (env === 'production'){
+    return common;
+  }
+  if (env === 'development'){
+    return merge([
+      common,
+      devserver(),
+      sass()
+    ])
+  }
 };
